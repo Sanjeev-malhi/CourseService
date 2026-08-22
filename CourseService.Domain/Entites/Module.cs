35bdf5edf9b0614720802f286f8bf23c36c7635b
Sanjeev <sanjeevmalhi9145@gmail.com>
2026-08-22 11:12:16 +0530

    Added Course Module Lesson Entites

---
+++ CourseService.Domain/Entites/Module.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseService.Domain.Entites
+{
+    public class Module
+    {
+        public Guid Id { get; set; }
+
+        public Guid CourseId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public int OrderIndex { get; set; }
+
+        public ICollection<Lesson> Lessons { get; set; } = new List<Lesson>();
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastModified { get; set; }
+
+        public Course Course { get; set; } = null!;
+    }
+}
